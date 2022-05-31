3634dd65b3279ff765a41cfd812e9122e0ee6c96
bjpc <brandonpineda4@gmail.com>
2022-05-31 16:54:33 -0600

    :art: [ADD] components

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <Suspense>
+    <template #default>
+      <Home />
+    </template>
+    <template #fallback>
+      <SplashScreen />
+    </template>
+  </Suspense>
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+import SplashScreen from "@/components/SplashScreen.vue";
+
+export default {
+  components: {
+    Home: defineAsyncComponent(
+      () =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(import("@/components/Home.vue"));
+          }, 2500);
+        })
+    ),
+    SplashScreen,
+  },
+};
+</script>
+
+<style>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <Layout>
+    <template #header>
+      <Header></Header>
+    </template>
+    <template #resume>
+      <Resume
+        :total-label="'Ahorro total'"
+        :label="label"
+        :total-amount="10000"
+        :amount="amount"
+      >
+        <template #graphic>graphic</template>
+        <template #action>action</template>
+      </Resume>
+    </template>
+    <template #movements>
+      <Movements />
+    </template>
+  </Layout>
+</template>
+
+<script>
+import Layout from "./Layout.vue";
+import Header from "./Header.vue";
+import Resume from "./Resume/Index.vue";
+import Movements from "./Movements.vue";
+
+export default {
+  components: {
+    Layout,
+    Header,
+    Resume,
+    Movements,
+  },
+  data() {
+    return {
+      label: null,
+      amount: null,
+    };
+  },
+};
+</script>
